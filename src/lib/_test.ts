f25de54bba9428bbd13adb9c12deb1d8f4818ef5
Andre0n <andreon098@gmail.com>
2023-01-25 14:30:38 -0300

    Add the `toBe` function in `Test` class
    - This function compares the result of function in test with the
      expected value.

--- src/lib/_test.ts
+++ src/lib/_test.ts
@@ -1,3 +1,5 @@
+import { matcher_options } from "./types";
+
 export default class Test {
   private description: string;
   sucess: boolean = false;
@@ -8,5 +10,18 @@ export default class Test {
   }
 
   expect(test_function: () => any) {
+    const current = test_function();
+    return {
+      toBe: (expected: any) => {
+        const options: matcher_options = {
+          matcher_name: "toBe",
+          comment: "Object.is equality",
+          expected: expected,
+          current: current,
+        };
+        const pass = Object.is(current, expected);
+        this.sucess = pass;
+      },
+    };
   }
 }
