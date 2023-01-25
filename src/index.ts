47ce6db80026bed9684f46f0a0ad6cd8e208b81b
Andre0n <andreon098@gmail.com>
2023-01-25 14:24:39 -0300

    Update index.ts to test the lib

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
+import { TestSuite } from "./lib";
 const main = () => {
-  console.log("Hello, World!");
-}
+  const my_suite = new TestSuite("Test numbers");
+  my_suite
+    .add_test(" 1 + 1 should be 2")
+    .expect(() => {
+      return 1 + 1;
+    })
+    .toBe(3);
 
+  my_suite
+    .add_test(" 'aabb' == 'aabb' ")
+    .expect(() => {
+      return "aabb";
+    })
+    .toBe("aabb");
+
+  my_suite.report();
+};
 main();
